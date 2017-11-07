582fcbfde14c9bd19b567ebbda3806e25c8c9a92
Etienne Delattre <ejm.delattre@gmail.com>
2017-11-07 18:11:46 +0100

    lifecycle Hooks

---
+++ MovieStreaminCore/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MovieStreaming
+{
+    using System;
+    using Akka.Actor;
+    using MovieStreaming.Actors;
+    using MovieStreaming.Messages;
+
+    class Program
+    {
+        private static ActorSystem _movieStreaminActorSystem;
+
+        static void Main()
+        {
+            _movieStreaminActorSystem = ActorSystem.Create("MovieStreaminActorSystem");
+            Console.WriteLine("Actor system created");
+
+            var playbackActorProps = Props.Create<PlaybackActor>();
+            var playbackActorRef = _movieStreaminActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+            
+            playbackActorRef.Tell(new PlayMovieMessage("Akka.NET: The Movie", 42));
+            playbackActorRef.Tell(new PlayMovieMessage("Partial Recall", 99));
+            playbackActorRef.Tell(new PlayMovieMessage("Boolean Lies", 77));
+            playbackActorRef.Tell(new PlayMovieMessage("Codenan the Destroyer", 1));
+
+            Console.ReadKey();
+            
+            _movieStreaminActorSystem.Terminate();
+
+            _movieStreaminActorSystem.WhenTerminated.Wait();
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
