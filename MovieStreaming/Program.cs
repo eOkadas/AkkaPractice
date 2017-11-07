7093ebce02cfe990876a564c3f61d1fff748b5b1
Etienne Delattre <ejm.delattre@gmail.com>
2017-11-07 16:12:24 +0100

    first pluralsight test

---
+++ MovieStreaming/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MovieStreaming
+{
+    using System;
+    using Akka.Actor;
+
+    class Program
+    {
+        private static ActorSystem MovieStreaminActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreaminActorSystem = ActorSystem.Create("MovieStreaminActorSystem");
+
+            Console.ReadLine();
+
+            MovieStreaminActorSystem.Terminate();
+        }
+    }
+}
